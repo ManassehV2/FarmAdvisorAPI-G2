0e8e8f9a26773fe901572acdaa7312815e764989
biruk_anley <birukanley2022@gmail.com>
2022-12-09 11:03:29 +0300

    adding interface for SensorDataStorageService

---
+++ src/FarmAdvisor.DataAccess.AzureTableStorage/Services/ISensorDataStorageService.cs
@@ -0,0 +1,12 @@
+﻿using FarmAdvisorApi.Models;
+using System.Threading.Tasks;
+
+namespace FarmAdvisor.DataAccess.AzureTableStorage.Services
+{
+    public interface ISensorDataStorage
+    {
+        Task<SensorEntity> GetEntityAsync(string category, string id);
+        Task<SensorEntity> UpsertEntityAsync(SensorEntity entity);
+        Task DeleteEntityAsync(string category, string id);
+    }
+}
