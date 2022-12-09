2d460c59cba9a6d3ceab2a94aa8b7c7fc192c861
biruk_anley <birukanley2022@gmail.com>
2022-12-09 11:02:47 +0300

    initializing a model for field

---
+++ src/FarmAdvisor.DataAccess.AzureTableStorage/Models/Field.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+
+namespace FarmAdvisorApi.Models
+{
+    public class FieldEntity : ITableEntity
+    {
+        public string? FieldId { get; set; }
+        public int? FarmId { get; set; }
+        public string? Name { get; set; }
+        public int Altitude { get; set; }
+        public string? Polygon { get; set; }
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
