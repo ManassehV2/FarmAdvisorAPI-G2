080448cca69e7c8f073565d0a2bc5f03646287e4
biruk_anley <birukanley2022@gmail.com>
2022-12-18 23:11:54 +0300

    update an interface for azurite table storage

--- src/FarmAdvisor.DataAccess.AzureTableStorage/test/IAzuritTableStorage.cs
+++ src/FarmAdvisor.DataAccess.AzureTableStorage/test/IAzuritTableStorage.cs
@@ -1,16 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
 using FarmAdvisorApi.Models;
 
-namespace StonePorch.Tests.Mocks
+namespace FarmAdvisor.DataAccess.AzureTableStorage.services
 {
-    public interface IAzuritTableStorage
-    {
 
-      
-        Task<CalculatedGddEntity> UpsertEntityAsync(CalculatedGddEntity entity);
-        Task<CalculatedGddEntity> DeleteEntityAsync(string SensorId, string CalculatedGdd);
-        Task<CalculatedGddEntity> GetEntityAsync<CalculatedGddEntity>(string SensorId , string CalculatedGdd );
-        Task<SensorDataStorageEntity> UpsertEntityAsync(SensorDataStorageEntity entity);
-        Task<WeatherForecastEntity> UpsertEntityAsync(WeatherForecastEntity entity);
-        
+    public interface IAzuriteTableStorage
+    {
+        Task<T> GetEntityAsync<T>(string PartitionKey, string RowKey) where T: class, ITableEntity, new();
+        Task<T> UpsertEntityAsync<T>(T entity) where T: class, ITableEntity, new();
+        Task DeleteEntityAsync<T>(string PartitionKey, string RowKey) where T: class, ITableEntity, new();
     }
 }
