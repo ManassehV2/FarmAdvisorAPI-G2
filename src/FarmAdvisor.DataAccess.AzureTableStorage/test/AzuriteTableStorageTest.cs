8128de2f9d2dc595466aa86c738f471b90c30209
biruk_anley <birukanley2022@gmail.com>
2022-12-18 23:11:35 +0300

    update azurite table storage test

--- src/FarmAdvisor.DataAccess.AzureTableStorage/test/AzuriteTableStorageTest.cs
+++ src/FarmAdvisor.DataAccess.AzureTableStorage/test/AzuriteTableStorageTest.cs
@@ -1,172 +1,91 @@
-using System;
-using System.Collections.Generic;
-using System.Data.Services.Client;
-using System.Linq;
-using FarmAdvisorApi.Models;
-using FarmAdvisor.DataAccess.AzureTableStorage.Services;
-using Microsoft.WindowsAzure.Storage.Table;
+using Xunit;
 
 
-namespace StonePorch.Tests.Mocks
+using FarmAdvisor.DataAccess.AzureTableStorage.services;
+
+
+namespace FarmAdvisor.Function.Test.DataAccess.AzureTableStorage
 {
-
-    public class AzuriteTableStorageTest : IAzuritTableStorage
+    public class AzureTableStorageTest
     {
-        private readonly Dictionary<string, Dictionary<string, Dictionary<string, object>>> _tables =
-            new Dictionary<string, Dictionary<string, Dictionary<string, object>>>();
-
-
-        /// Creates a new table if it does not already exist.
-
-        public bool CreateTable(string tableName)
+        public AzureTableStorageTest(string tableName)
         {
-            if (TableExists(tableName))
-                return false;
-
-            _tables.Add(tableName, new Dictionary<string, Dictionary<string, object>>());
-            return true;
+            TableName = tableName;
         }
 
-        // Deletes a table if it exists.
+        public string TableName { get; }
 
-        public bool DeleteTable(string tableName)
+        [Fact]
+        public async Task TestConnection()
         {
-            return _tables.Remove(tableName);
-        }
-
-        /// Checks whether the specified table exists.
-        public bool TableExists(string tableName)
-        {
-            return _tables.ContainsKey(tableName);
-        }
-
-
-        // Adds the specified entity into the table.
-
-        // True if the entity has been successfully added, otherwise False.
-
-        public bool Add<T>(string tableName, T entity)
-        {
-            string partitionKey, rowKey;
-            GetTableEntityKeys(entity, out partitionKey, out rowKey);
-            var table = _tables[tableName];
-            if (!table.ContainsKey(partitionKey))
+            string tableName = "Weather ";
+            var storage = new AzureTableStorageTest(tableName);
+            try
             {
-                table.Add(partitionKey, new Dictionary<string, object>());
+                await storage.DeleteEntityAsync<Weather>("del", "del");
+                Assert.True(false);
             }
-            if (table[partitionKey].ContainsKey(rowKey))
+            catch
             {
-                return false;
+                Assert.True(true);
             }
-            table[partitionKey].Add(rowKey, entity);
-            return true;
         }
 
-        // Updates the specified entity stored in the table.
-
-        // True if the entity has been successfully updated, otherwise False.
-
-        public bool Update<T>(string tableName, T entity)
-        {
-            string partitionKey, rowKey;
-            GetTableEntityKeys(entity, out partitionKey, out rowKey);
-            var table = _tables[tableName];
-            if (!table.ContainsKey(partitionKey))
-            {
-                return false;
-            }
-            if (!table[partitionKey].ContainsKey(rowKey))
-            {
-                return false;
-            }
-            table[partitionKey][rowKey] = entity;
-            return true;
-        }
-
-
-
-        // upserting checher
-        /// True if the entity has been successfully updated or inserted, otherwise False.
-        /// </returns>
-        public bool Upsert<T>(string tableName, T entity, bool replace)
+        private Task DeleteEntityAsync<T>(string v1, string v2)
         {
             throw new NotImplementedException();
         }
 
-
-        /// Deletes the specified entity stored in the table.
-
-        // True if the entity has been successfully deleted, otherwise False.
-        // </returns>
-        public bool Delete<T>(string tableName, T entity)
+        [Fact]
+        public async Task UpsertAndGetEntityTest()
         {
-            string partitionKey, rowKey;
-            GetTableEntityKeys(entity, out partitionKey, out rowKey);
-            var table = _tables[tableName];
-            if (!table.ContainsKey(partitionKey))
-            {
-                return false;
-            }
-            if (table[partitionKey].Remove(rowKey))
-            {
-                return true;
-            }
-            // Azure actually throws an exception if the entity doesn't exist
-            throw new DataServiceRequestException("An error occurred while processing this request.",
-                                                  new DataServiceClientException(@"<?xml version=""1.0"" encoding=""utf-8"" standalone=""yes""?>
-    <error xmlns=""http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"">
-    <code>ResourceNotFound</code>
-    <message xml:lang=""en-GB"">The specified resource does not exist.</message>
-    </error>", 404));
+            string tableName = "Weather";
+            var storage = new TableStorageService(tableName);
+            var weather = new Weather();
+            weather.PartitionKey = "10";
+            weather.RowKey = "01";
+
+
+            var result =  await storage.UpsertEntityAsync<Weather>(weather);
+
+            var added = await storage.GetEntityAsync<Weather>(weather.PartitionKey, weather.RowKey);
+            Assert.Equal(result.Temprature, added.Temprature);
+            Assert.Equal(result.PartitionKey, added.PartitionKey);
+            Assert.Equal(result.RowKey, added.RowKey);
+
+
+           
+            
         }
 
-
-
-        private static bool GetTableEntityKeys<T>(T entity, out string partitionKey, out string rowKey)
+        [Fact]
+        public async Task DeleteEntityTest()
         {
-            var tableEntity = (object)entity as TableEntity;
-            if (tableEntity != null)
-            {
-                partitionKey = tableEntity.PartitionKey;
-                rowKey = tableEntity.RowKey;
-                return !string.IsNullOrEmpty(partitionKey);
-            }
+            string tableName = "Weather";
+            var storage = new TableStorageService(tableName);
+            var weather = new Weather();
+            weather.Temprature = 20;
+            weather.PartitionKey = "10";
+            weather.RowKey = "01";
 
-            // TODO
-            throw new NotImplementedException("Can't get entity keys from type " + typeof(T).Name);
+            await storage.UpsertEntityAsync<Weather>(weather);
+            await storage.DeleteEntityAsync<Weather>(weather.PartitionKey, weather.RowKey);
+
+            await Assert.ThrowsAsync<Azure.RequestFailedException>(async () => await storage.GetEntityAsync<Weather>(weather.PartitionKey, weather.RowKey));
+
+            // Assert.Null(removed);
+        }
+    }
+
+    internal class Weather
+    {
+        internal string PartitionKey;
+
+        public Weather()
+        {
         }
 
-        Task<CalculatedGddEntity> IAzuritTableStorage.UpsertEntityAsync(CalculatedGddEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<CalculatedGddEntity> IAzuritTableStorage.DeleteEntityAsync(string SensorId, string CalculatedGdd)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<CalculatedGddEntity> IAzuritTableStorage.GetEntityAsync<CalculatedGddEntity>(string SensorId, string CalculatedGdd)
-
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<SensorDataStorageEntity> IAzuritTableStorage.UpsertEntityAsync(SensorDataStorageEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-
-
-        Task<WeatherForecastEntity> IAzuritTableStorage.UpsertEntityAsync(WeatherForecastEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-
-
-
-
+        public string RowKey { get; internal set; }
+        public int Temprature { get; internal set; }
     }
 }
