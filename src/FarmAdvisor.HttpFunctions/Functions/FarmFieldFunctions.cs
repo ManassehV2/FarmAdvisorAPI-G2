115dde79a4e6c9dea88b181f0fdb9a3594060cf2
Abel <abelmekonen559@gmail.com>
2023-01-22 11:25:09 +0300

    [fix] fix errors

--- src/FarmAdvisor.HttpFunctions/Functions/FarmFieldFunctions.cs
+++ src/FarmAdvisor.HttpFunctions/Functions/FarmFieldFunctions.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Text.Json;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -10,8 +12,8 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System.Linq;
 using FarmAdvisor.Models.Models;
-
 
 namespace FarmAdvisor.HttpFunctions.Functions
 {
@@ -22,6 +24,7 @@ namespace FarmAdvisor.HttpFunctions.Functions
         public FarmFieldFunctions(ILogger<FarmFieldFunctions> logger)
         {
             _logger = logger;
+           
         }
         [FunctionName("GetAllFarmFields")]
         [OpenApiOperation(operationId: "GetAllFarmFields", tags: new[] { "FarmFields" }, Summary = "Gets all fields in a farm.", Description = "Gets all fields in a farm.", Visibility = OpenApiVisibilityType.Important)]
@@ -30,18 +33,15 @@ namespace FarmAdvisor.HttpFunctions.Functions
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<FarmFieldModel>), Description = "List of fields in a farm")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
         public async Task<IActionResult> GetAllFarmFields(
-             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "users/farms/fields")] HttpRequest req)
+             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "users/fields")] HttpRequest req)
         {
             _logger.LogInformation("Executing {method}", nameof(GetAllFarmFields));
-            string farmId = req.Query["farmId"];
 
-            if (Guid.TryParse(farmId, out Guid id))
-            {
+           
                 var result = new FarmFieldModel();
                 return new OkObjectResult(result);
-            }
+            
 
-            return new BadRequestObjectResult("Invalid Farm Id");
         }
 
        
