57a152dc304e90b04a8b1d68fc4ffec05800e90f
Abel <abelmekonen559@gmail.com>
2023-02-12 19:42:33 +0300

    setup first time user endpoints

--- src/FarmAdvisor.HttpFunctions/Functions/FarmFieldFunctions.cs
+++ src/FarmAdvisor.HttpFunctions/Functions/FarmFieldFunctions.cs
@@ -10,33 +10,65 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using FarmAdvisor.Models.Models;
+using FarmAdvisor.Business;
+using FarmAdvisor.DataAccess.MSSQL.Abstractions;
+using FarmAdvisor.DataAccess.MSSQL.Implementations;
+using System;
 
 namespace FarmAdvisor.HttpFunctions.Functions
 {
     public class FarmFieldFunctions
     {
-        private readonly ILogger<FarmFieldFunctions> _logger;
+        private FarmFieldService _farmFieldService;
 
-        public FarmFieldFunctions(ILogger<FarmFieldFunctions> logger)
+        public FarmFieldFunctions( )
         {
-            _logger = logger;
-           
+            _farmFieldService = new FarmFieldService(new UnitOfWorkImpl());
         }
+        
         [FunctionName("GetAllFarmFields")]
         [OpenApiOperation(operationId: "GetAllFarmFields", tags: new[] { "FarmFields" }, Summary = "Gets all fields in a farm.", Description = "Gets all fields in a farm.", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Header)]
-        [OpenApiParameter(name: "farmId", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The fields farm Id")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<FarmFieldModel>), Description = "List of fields in a farm")]
         [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
         public async Task<IActionResult> GetAllFarmFields(
-             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "users/fields")] HttpRequest req)
+             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "farmFields")] HttpRequest req)
         {
-            _logger.LogInformation("Executing {method}", nameof(GetAllFarmFields));
-           
-                var result = new FarmFieldModel();
+
+                var result = await _farmFieldService.GetAllFarmFields();
                 return new OkObjectResult(result);
 
         }
+
+        [FunctionName("CreateFarmField")]
+        [OpenApiOperation(operationId: "CreateFarmField", tags: new[] { "CreateFarmField" }, Summary = "CreateFarmField.", Description = "CreateFarmField.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Header)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<FarmFieldModel>), Description = "List of fields in a farm")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.Created, Summary = "Unable to Create Farm Field", Description = "Unable to Create Farm Field")]
+        public async Task<IActionResult> CreateFarmField(
+             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "farmFields")] HttpRequest req)
+        {
+                var farmField = new FarmFieldModel(null, req.Form["name"], decimal.Parse(req.Form["altitude"]), new Guid(req.Form["farmId"]));
+                var result = await _farmFieldService.CreateFarmField(farmField);
+                return new OkObjectResult(result);
+        }
+
+        [FunctionName("GetFarmFieldsByFarmId")]
+        [OpenApiOperation(operationId: "GetAllFarmFields", tags: new[] { "FarmFields" }, Summary = "Gets all fields in a farm.", Description = "Gets all fields in a farm.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Header)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<FarmFieldModel>), Description = "List of fields in a farm")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
+        public async Task<IActionResult> GetFarmFieldsByFarmId(
+             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "farms/farmFields/{farmId}")] HttpRequest req, Guid farmId)
+        {
+            try{
+                var result = await _farmFieldService.GetFarmFieldsByFarmId(farmId);
+                return new OkObjectResult(result);
+            }catch(Exception ex){
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
 
     }
 }
--- src/FarmAdvisor.HttpFunctions/HttpFunctionStartup.cs
+++ src/FarmAdvisor.HttpFunctions/HttpFunctionStartup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using FarmAdvisor.Services.WeatherApi;
-
+using Microsoft.EntityFrameworkCore;
 
 [assembly: FunctionsStartup(typeof(FarmAdvisor.HttpFunctions.HttpFunctionStartup))]
 
@@ -12,11 +12,29 @@ namespace FarmAdvisor.HttpFunctions
 {
     public class HttpFunctionStartup : FunctionsStartup
     {
-        public static void ConfigureServices(IServiceCollection services)
+       
+        public  void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient<IWeatherRemoteRepository>();
+            var ConfigBuilder = new ConfigurationBuilder();
+            ConfigBuilder.AddJsonFile("local.settings.json", optional: false);
+            ConfigBuilder.AddEnvironmentVariables();
+            var config = ConfigBuilder.Build();
+            
+
+
+
+            var  connectionString = "Data Source=LAPTOP-7S5M2IVT;Initial Catalog=FarmAdvisorDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+           
+            services.AddDbContext<DataAccess.MSSQL.FarmAdvisorDbContext>(options =>
+                options.UseSqlServer(connectionString!, b => b.MigrationsAssembly(typeof(DataAccess.MSSQL.FarmAdvisorDbContext).Assembly.FullName)), ServiceLifetime.Transient);
+            services.AddTransient<DataAccess.MSSQL.Abstractions.IUnitOfWork, DataAccess.MSSQL.Implementations.UnitOfWorkImpl>();
             services.AddScoped<IWeatherRemoteRepository, WeatherRemoteRepositoryImpl>();
 
+            services.AddScoped<Business.FarmService>();
+            services.AddScoped<Business.UserService>();
+            services.AddTransient<Business.FarmFieldService>();
+
+            services.AddHttpClient<IWeatherRemoteRepository>();
 
         }
 
